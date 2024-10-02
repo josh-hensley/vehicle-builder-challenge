0c2d49b900bf905bf4864c6c6a18788c960752e0
josh-hensley <me@joshhensley.com>
2024-10-02 17:28:44 -0500

    moved mostly complete challenge into new dir

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import Truck from "./classes/Truck.js";
+import Car from "./classes/Car.js";
+import Motorbike from "./classes/Motorbike.js";
+import Wheel from "./classes/Wheel.js";
+import Cli from "./classes/Cli.js";
+
+const vehicles = [];
+
+const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+
+// will use default wheels
+const car1 = new Car(
+  Cli.generateVin(),
+  'blue',
+  'Toyota',
+  'Camry',
+  2021,
+  3000,
+  130,
+  []
+);
+
+const motorbike1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
+const motorbike1 = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels);
+
+
+vehicles.push(car1);
+vehicles.push(motorbike1);
+vehicles.push(truck1);
+
+const cli = new Cli(vehicles);
+
+cli.startCli();
